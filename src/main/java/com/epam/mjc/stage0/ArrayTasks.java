c9d508b7bebfc66814b2cd3eba7909e412cfc992
8ar7ar <5ar7ar@mail.ru>
2023-10-26 00:45:51 +0500

    stage0-module5-task1 "1st attempt" "raggedArray not completed" "locally tested"

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-
+        return new String[]{"winter", "spring", "summer", "autumn", };
     }
 
     /**
@@ -25,7 +25,9 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) arr[i] = i +1;
+        return arr;
     }
 
     /**
@@ -37,7 +39,9 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int sum = 0;
+        for (int j : arr) sum += j;
+        return sum;
     }
 
     /**
@@ -50,7 +54,8 @@ public class ArrayTasks {
      * arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        for (int i = 0; i < arr.length; i++) if (arr[i] == number) return i;
+        return -1;
     }
 
     /**
@@ -63,7 +68,9 @@ public class ArrayTasks {
      * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-
+        String[] reverse = new String[arr.length];
+        for (int i = 0; i < arr.length ; i++) reverse[i] = arr[arr.length-1-i];
+        return reverse;
     }
 
     /**
@@ -78,8 +85,17 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
+        int newArrayLength = 0;
+        int newArrayIndex = 0;
+        for (int j : arr) if (j > 0) newArrayLength++;
 
+        int[] newArray = new int[newArrayLength];
+
+        for (int j : arr) if (j > 0) newArray[newArrayIndex++] = j;
+
+        return newArray;
     }
+
 
     /**
      * Return a sorted, ragged, two-dimensional int[][] array following these rules:
@@ -92,6 +108,27 @@ public class ArrayTasks {
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length-1; j++) {
+                if (arr[i].length > arr[j].length) {
+                    int[] tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
 
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length == 1) continue;
+            for (int j = 0; j < arr[i].length-1; j++) {
+                if (arr[i][j] > arr[i][j+1]) {
+                    int temp = arr[i][j];
+                    arr[i][j] = arr[i][j+1];
+                    arr[i][j+1] = temp;
+                }
+            }
+        }
+
+        return arr; // temporary array
     }
 }
